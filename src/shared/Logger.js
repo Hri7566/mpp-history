cb03072a1d8cf75bee650491d19a4e2f5d742131
Hri7566 <hri7566@gmail.com>
2022-04-09 19:43:59 -0400

    add timestamps to log

--- src/shared/Logger.js
+++ src/shared/Logger.js
@@ -4,7 +4,8 @@ let logLevelColors = {
     'warn': '\x1b[33m',
     'error': '\x1b[31m',
     'fatal': '\x1b[31m',
-    'reset': '\x1b[0m'
+    'reset': '\x1b[0m',
+    'time': '\x1b[32m'
 };
 
 class Logger {
@@ -13,24 +14,32 @@ class Logger {
         this.color = color || '\x1b[36m';
     }
 
+    static getHHMMSS() {
+        let date = new Date();
+        let hh = date.getHours();
+        let mm = date.getMinutes();
+        let ss = date.getSeconds();
+        return `${logLevelColors['time']}${(hh < 10 ? '0' : '') + hh}:${(mm < 10 ? '0' : '') + mm}:${(ss < 10 ? '0' : '') + ss}${logLevelColors['reset']}`;
+    }
+
     log(...args) {
-        console.log(`[${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
+        console.log(`${Logger.getHHMMSS()} [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
     }
 
     warn(...args) {
-        console.log(`[${logLevelColors['warn']}WARN${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
+        console.log(`${Logger.getHHMMSS()} [${logLevelColors['warn']}WARN${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
     }
 
     error(...args) {
-        console.log(`[${logLevelColors['error']}ERROR${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
+        console.log(`${Logger.getHHMMSS()} [${logLevelColors['error']}ERROR${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
     }
 
     fatal(...args) {
-        console.log(`[${logLevelColors['fatal']}FATAL${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
+        console.log(`${Logger.getHHMMSS()} [${logLevelColors['fatal']}FATAL${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
     }
 
     debug(...args) {
-        console.log(`[${logLevelColors['debug']}DEBUG${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
+        console.log(`${Logger.getHHMMSS()} [${logLevelColors['debug']}DEBUG${logLevelColors['reset']}] [${this.color}${this.id}${logLevelColors['reset']}]`, ...args);
     }
 }
 
