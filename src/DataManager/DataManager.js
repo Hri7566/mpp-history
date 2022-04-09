6c3c4442f08f275b70d8bf45441877954dfec6f5
Hri7566 <hri7566@gmail.com>
2022-04-09 16:50:42 -0400

    permissions

--- src/DataManager/DataManager.js
+++ src/DataManager/DataManager.js
@@ -2,10 +2,10 @@ const { Level } = require('level');
 const { Logger } = require('../shared/Logger');
 const SuperKeys = require('./SuperKeys');
 
-class User {
+class HistoryUser {
     constructor(p) {
         //! must be JSON-compatible 24/7
-        if (!User.isParticipant(p)) return;
+        if (!HistoryUser.isParticipant(p)) return;
         this.participantHistory = [p];
         this.latestUpdate = Date.now();
     }
@@ -27,7 +27,7 @@ class User {
 
     /**
      * Get a user's latest participant object
-     * @param {User} u User to get data from
+     * @param {HistoryUser} u User to get data from
      * @returns {*} Participant data
      */
     static getLatestParticipant(u) {
@@ -37,7 +37,7 @@ class User {
 
     /**
      * Update a user's participant history
-     * @param {User} u User to update
+     * @param {HistoryUser} u User to update
      * @param {*} p Participant data to append
      */
     static updateUser(u, p) {
@@ -46,11 +46,71 @@ class User {
     }
 }
 
+class Permission {
+    /**
+     * Check two permissions for equality
+     * @param {string} p1 Permission
+     * @param {string} p2 Permission
+     * @returns boolean
+     */
+    static checkPermissions(p1, p2) {
+        p1 = p1.split('.');
+        p2 = p2.split('.');
+
+        for (let i = 0; i < p1.length; i++) {
+            if (p1[i] == '*' || p2[i] == '*') continue;
+            if (p1[i] != p2[i]) return false;
+        }
+
+        return true;
+    }
+}
+
+class PermissionGroup {
+    static groups = {
+        'DEFAULT': new PermissionGroup('DEFAULT', [
+            'command.help'
+        ]),
+        'ADMIN': new PermissionGroup('ADMIN', [
+            '*'
+        ])
+    };
+
+    constructor(id, permissions) {
+        this.id = id;
+        this.permissions = permissions;
+    }
+    
+    static getGroup(id) {
+        return PermissionGroup.groups[id];
+    }
+
+    /**
+     * Check if a group has a permission
+     * @param {string} group String ID of group to check
+     * @param {string} perm Permission
+     */
+    static checkGroupPermission(group, perm) {
+        let groupObj = PermissionGroup.groups[group];
+        if (!groupObj) return false;
+        return groupObj.permissions.some(p => Permission.checkPermissions(p, perm));
+    }
+}
+
+class PermissionUser {
+    constructor(id, groups, permissions) {
+        //! must be JSON-compatible
+        this.id = id;
+        this.groups = groups || ['DEFAULT'];
+        this.permissions = permissions || [];
+    }
+}
+
 class DataManager {
     static logger = new Logger('Data', '\x1b[34m');
 
     // Handle every data request here
-    static userDataDB;
+    static db;
     static keys = new Map();
 
     /**
@@ -58,7 +118,7 @@ class DataManager {
      */
     static async start() {
         this.logger.log('Starting DataManager...');
-        this.userDataDB = new Level('./data/history.db', { valueEncoding: 'json' });
+        this.db = new Level('./data/history.db', { valueEncoding: 'json' });
         await this.setDefaultData();
         process.send('ready');
     }
@@ -68,7 +128,7 @@ class DataManager {
      */
     static async stop() {
         this.logger.log('Stopping DataManager...');
-        await this.userDataDB.close();
+        await this.db.close();
         process.send('stopped');
     }
 
@@ -79,7 +139,7 @@ class DataManager {
 
     static putSetting(subkey, val) {
         return new Promise((resolve, reject) => {
-            this.userDataDB.put(SuperKeys['settings'] + subkey, val, (err) => {
+            this.db.put(SuperKeys['settings'] + subkey, val, (err) => {
                 if (err) resolve(err);
                 else resolve();
             });
@@ -88,7 +148,7 @@ class DataManager {
 
     static getSetting(subkey) {
         return new Promise((resolve, reject) => {
-            this.userDataDB.get(SuperKeys['settings'] + subkey, (err, val) => {
+            this.db.get(SuperKeys['settings'] + subkey, (err, val) => {
                 if (err) resolve(err);
                 else resolve(undefined, val);
             });
@@ -97,7 +157,25 @@ class DataManager {
 
     static settingExists(subkey) {
         return new Promise((resolve, reject) => {
-            this.userDataDB.get(SuperKeys['settings'] + subkey, (err, val) => {
+            this.db.get(SuperKeys['settings'] + subkey, (err, val) => {
+                if (err) resolve(err);
+                else resolve(undefined, val);
+            });
+        });
+    }
+
+    static putPermissionUser(pu, val) {
+        return new Promise((resolve, reject) => {
+            this.db.put(SuperKeys['user_permissions'] + pu.id, val, (err) => {
+                if (err) resolve(err);
+                else resolve();
+            });
+        });
+    }
+
+    static getPermissionUser(id) {
+        return new Promise((resolve, reject) => {
+            this.db.get(SuperKeys['user_permissions'] + id, (err, val) => {
                 if (err) resolve(err);
                 else resolve(undefined, val);
             });
