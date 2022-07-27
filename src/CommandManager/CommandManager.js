c1ac3e34df216ee8b9c8da0a8c66ede9b3d4d732
Hri7566 <hri7566@gmail.com>
2022-07-27 07:14:40 -0400

    engineer color command

--- src/CommandManager/CommandManager.js
+++ src/CommandManager/CommandManager.js
@@ -1,3 +1,9 @@
+/**
+ * TODO natural language processing (nlp)
+ */
+
+const Color = require('../shared/Color');
+
 const { Logger } = require('../shared/Logger');
 const { EventEmitter } = require('events');
 
@@ -41,7 +47,12 @@ class Command {
             }
         }
 
+        if (!found_cmd) return;
         this.removeCommand(found_cmd);
+    }
+
+    static getUsage(usage, prefix) {
+        return usage.split('%P').join(prefix);
     }
 }
 
@@ -133,25 +144,64 @@ process.on('message', async (msg) => {
     await CommandManager.receiveServerMessage(msg);
 });
 
-Command.addCommand(new Command('help', ['help', 'h', 'cmds'], '%Phelp', 'Displays all commands', async (msg, platform) => {
-    let list = "Commands:";
+Command.addCommand(new Command('help', ['help', 'h', 'cmds'], '%Phelp [command]', `Displays all commands, or will display a command's description and usage if provided. Usage guide: literal text <required argument> [optional argument] (required|one|or|the|other) [optional|one|or|the|other]`, async (msg, platform) => {
+    if (!msg.args[1]) {
+        let list = "Commands:";
 
-    for (let cmd of Object.values(Command.commands)) {
-        if (cmd.dependent !== 'independent') {
-            if (platform !== cmd.dependent) continue;
+        for (let cmd of Object.values(Command.commands)) {
+            if (cmd.dependent !== 'independent') {
+                if (platform !== cmd.dependent) continue;
+            }
+
+            list += ` ${msg.prefix}${cmd.acc[0]} |`;
         }
 
-        list += ` ${msg.prefix}${cmd.acc[0]} |`;
+        list = list.substring(0, list.length - 1).trim();
+
+        return list;
+    } else {
+        let usage_desc;
+        let found_cmd;
+        let argcat = msg.a.substring(msg.args[0].length).trim();
+
+        cmdLoop:
+        for (let cmd of Object.values(Command.commands)) {
+            if (cmd.dependent !== 'independent') {
+                if (platform !== cmd.dependent) continue;
+            }
+
+            for (let a of cmd.acc) {
+                if (a.toLowerCase().includes(argcat.toLowerCase()) || a.toLowerCase().includes(msg.args[1].tolowerCase())) {
+                    found_cmd = cmd;
+                    break cmdLoop;
+                }
+            }
+        }
+
+        if (!found_cmd) {
+            return `There is no usage for the command '${argcat}'.`;
+        }
+
+        return `Usage: ${Command.getUsage(found_cmd.usage, msg.prefix)}| ${found_cmd.desc}`;
     }
-
-    list = list.substring(0, list.length - 1).trim();
-
-    return list;
 }));
 
 Command.addCommand(new Command('about', ['about', 'a'], '%Pabout', 'Returns information about this bot', async (msg, platform) => {
     return `This bot will eventually be used to search user data more thoroughly than Theta. MPP History - by Hri7566 (not to be confused with JPDLD's Bot)`;
-}))
+}));
+
+Command.addCommand(new Command('color', ['color', 'c'], '%Pcolor [color]', 'Returns information regarding the proveded color (or your own, if no arguments are passed)', async (msg, platform) => {
+    if (!msg.args[1]) {
+        return `${msg.p.name}, your color is ${new Color(msg.p.color).getName().toLowerCase()}.`;
+    }
+
+    if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}$)/i.test(msg.args[1])) {
+        let c = new Color(msg.args[1]);
+        return `That hex code (${c.toHexa()}) looks like ${c.getName().toLowerCase()}.`
+    } else {
+        return `Excuse me, I don't think '${msg.args[1]}' is a hex code. (Maybe you forgot '#'?)`;
+    }
+}));
 
 module.exports = {
     CommandManager,
